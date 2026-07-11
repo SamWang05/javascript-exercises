b920a0a5e39c669e3842e9e8711da365de903fe8
SamWang05 <wangsam00207@gmail.com>
2026-07-11 11:58:29 -0400

    completed exercise 15

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(booksArray) {
+    let titlesArray = [];
 
+    booksArray.forEach((book) => {
+        titlesArray.push(book.title);
+    });
+
+    return titlesArray;
 };
 
 // Do not edit below this line
