6d64c44944608ba961991f081e8e72ea64b0836c
SamWang05 <wangsam00207@gmail.com>
2026-07-11 11:07:49 -0400

    finished exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayNums) {
+  let sum = 0;
+
+	for (let i = 0; i < arrayNums.length; i++) {
+    sum = sum + arrayNums[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arrayNums) {
+  let product = 1;
 
+  if (arrayNums.length == 0) return 0;
+
+	for (let i = 0; i < arrayNums.length; i++) {
+    product = product * arrayNums[i];
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorialResult = 1;
+
+  if (num == 0) return 1;
+
+  for (let i = num; i > 0; i--) {
+    factorialResult = factorialResult * i;
+  }
+
+  return factorialResult;
 };
 
 // Do not edit below this line
