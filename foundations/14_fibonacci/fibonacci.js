958ad11ac76f3400f28f4bb4a7a150db3d996f84
SamWang05 <wangsam00207@gmail.com>
2026-07-11 11:53:04 -0400

    finished exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let acceptedValue = index;
 
+    if (index < 0) return "OOPS";
+    if (typeof(acceptedValue) == "string") acceptedValue = parseInt(acceptedValue, 10);
+
+    let firstNum = 1;
+    let secondNum = 1;
+    let newNum = 0;
+
+    if (acceptedValue == 1 || acceptedValue == 2) return 1;
+
+    for (let i = 3; i <= acceptedValue; i++) {
+        newNum = firstNum + secondNum;
+
+        firstNum = secondNum;
+        secondNum = newNum;
+    }
+
+    return newNum;
 };
 
 // Do not edit below this line
