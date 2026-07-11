a96aea0ce3889bd61ca740cfc72415bce0888c89
SamWang05 <wangsam00207@gmail.com>
2026-07-11 13:29:13 -0400

    completed exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleObjsArray) {
+    let oldestPerson;
+    let oldestPersonAge = 0;
 
+    peopleObjsArray.forEach((person) => {
+        let birthYear = person.yearOfBirth;
+        let deathYear = person.yearOfDeath;
+        const currentDate = new Date();
+
+        if (deathYear == undefined) deathYear = currentDate.getFullYear();
+
+        let age = deathYear - birthYear;
+
+        console.log(age);
+
+        if (age > oldestPersonAge) {
+            oldestPersonAge = age;
+            oldestPerson = person;
+        }
+    });
+
+    return oldestPerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
