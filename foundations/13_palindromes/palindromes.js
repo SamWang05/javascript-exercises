cf9c830cbc3f18573ac6b7c1cc6a7c62f9827ddc
SamWang05 <wangsam00207@gmail.com>
2026-07-11 11:42:37 -0400

    completed exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (targetString) {
+    let cleanedString = targetString.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    
+    let reverseString = cleanedString.split("").reverse().join("");
 
+    if (cleanedString == reverseString) return true;
+    else return false;
 };
 
 // Do not edit below this line
